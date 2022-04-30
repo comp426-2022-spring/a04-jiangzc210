3f08a7af39e7c394ce9c7d7e2fa6a4e382108e0f
jiangzc <jiangzc@ad.unc.edu>
2022-04-29 22:49:08 -0400

    implement --help option

--- server.js
+++ server.js
@@ -3,7 +3,28 @@ const express = require('express')
 const app = express()
 const args = require('minimist')(process.argv.slice(2))
 args['port']
-const HTTP_PORT = args.port || 5000
+const HTTP_PORT = args.port || 5555
+
+const helpText = `
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.`;
+
+if (args['help']) {
+	console.log(helpText);
+	process.exit(0);
+}
 
 // Start an app server
 const server = app.listen(HTTP_PORT, () => {
